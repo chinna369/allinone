e9117b5021a8e8b7408d31dbf61d310cde777d84
chinna369 <p.chinna369@gmail.com>
2024-03-30 19:30:18 +0530

    new changes

--- src/app/tabs/home/home.page.ts
+++ src/app/tabs/home/home.page.ts
@@ -6,10 +6,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.page.scss'],
 })
 export class HomePage implements OnInit {
-
+  products: any[] = [];
   constructor() { }
 
   ngOnInit() {
+    this.products = [
+      {image: '../../../assets/images/fig3.jpg'},
+      {image: '../../../assets/images/fig1.jpg'},
+      {image: '../../../assets/images/fig2.jpg'},
+      {image: '../../../assets/images/fig4.jpg'},
+    ]
   }
 
 }
